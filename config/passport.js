d0f025a5874926ebeb52466b6e4bfc8d7ae7e0a6
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2025-10-09 23:17:28 +0300

    passport fix

--- config/passport.js
+++ config/passport.js
@@ -1,27 +1,49 @@
+// config/passport.js
 const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
 const User = require('../models/user');
 
-module.exports = (passport) => {
+module.exports = function(passport) {
+  // GitHub OAuth Strategy
   passport.use(
     new GitHubStrategy(
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `${process.env.BASE_URL}/auth/github/callback`
+        callbackURL: process.env.GITHUB_CALLBACK_URL || '/auth/github/callback'
       },
       async (accessToken, refreshToken, profile, done) => {
-        let user = await User.findOne({ githubId: profile.id });
-        if (!user) {
-          user = new User({ username: profile.username, githubId: profile.id });
-          await user.save();
+        try {
+          // Check if user exists
+          let user = await User.findOne({ githubId: profile.id });
+          if (!user) {
+            // Create new user if not found
+            user = await User.create({
+              githubId: profile.id,
+              username: profile.username,
+              email: profile.emails?.[0]?.value || `${profile.username}@github.com`
+            });
+          }
+          return done(null, user);
+        } catch (err) {
+          return done(err, null);
         }
-        return done(null, user);
       }
     )
   );
 
-  passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  // Serialize user to session
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  // Deserialize user from session
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id); // <- correct for latest Mongoose
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
   });
 };
