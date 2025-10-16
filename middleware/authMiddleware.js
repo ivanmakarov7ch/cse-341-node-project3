d619fde118be7ebeea94ac54eb64ec07943a1f8f
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2025-10-16 20:41:26 +0300

    Fixes auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
-function ensureAuth(req, res, next) {
-  if (req.isAuthenticated && req.isAuthenticated()) return next();
-  res.status(401).json({ message: 'Login required' });
-}
-
-module.exports = { ensureAuth };
+module.exports = function ensureAuth(req, res, next) {
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({ error: 'Unauthorized' });
+};
