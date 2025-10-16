bda5a8bd0420d623d7a3f25ad649c332a01f294d
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2025-10-16 20:50:35 +0300

    Fixed userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,29 +1,23 @@
 const express = require('express');
-const userController = require('../controllers/userController');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController'); 
+
+const { ensureAuth } = require('../middleware/authMiddleware'); 
+
 const router = express.Router();
+
 
 router.get('/', ensureAuth, getAllUsers);
 router.get('/:id', ensureAuth, getUserById);
 router.put('/:id', ensureAuth, updateUser);
 router.delete('/:id', ensureAuth, deleteUser);
+
+
 router.post('/', createUser);
+
 module.exports = router;
-
-
-
-
-
-// const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
-// const ensureAuth = require('../middleware/auth');
-
-
-
-// // Protect these routes
-// router.get('/', ensureAuth, getAllUsers);
-// router.get('/:id', ensureAuth, getUserById);
-
-
-// // Creating a user (register) is public
-// router.post('/', createUser);
-
-// module.exports = router;
