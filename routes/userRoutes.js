58898de0336fbcd4c71ded04e61cbb3153a2f012
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2025-10-16 20:46:37 +0300

    Fixing auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
-const ensureAuth = require('../middleware/auth');
-
+const userController = require('../controllers/userController');
 const router = express.Router();
-
 
 router.get('/', ensureAuth, getAllUsers);
 router.get('/:id', ensureAuth, getUserById);
 router.put('/:id', ensureAuth, updateUser);
 router.delete('/:id', ensureAuth, deleteUser);
-
 router.post('/', createUser);
-
 module.exports = router;
+
+
+
+
+
+// const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
+// const ensureAuth = require('../middleware/auth');
+
+
+
+// // Protect these routes
+// router.get('/', ensureAuth, getAllUsers);
+// router.get('/:id', ensureAuth, getUserById);
+
+
+// // Creating a user (register) is public
+// router.post('/', createUser);
+
+// module.exports = router;
